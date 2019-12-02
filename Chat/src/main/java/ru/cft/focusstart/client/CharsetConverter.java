16470a6327064d8c0aa06643dbe4eeb7275b074c
bruyroman <bruyroman@ya.ru>
2019-12-02 22:17:05 +0700

    - добавлен класс для конвертации кодировок  - добавлено окно подключения

---
+++ Chat/src/main/java/ru/cft/focusstart/client/CharsetConverter.java
@@ -0,0 +1,24 @@
+package ru.cft.focusstart.client;
+
+import java.io.UnsupportedEncodingException;
+
+public class CharsetConverter {
+
+    private CharsetConverter() {}
+
+    public static String cp1251ToUtf8(String s) {
+        try {
+            return new String(s.getBytes("CP1251"), "UTF8");
+        } catch (UnsupportedEncodingException e) {
+            return s;
+        }
+    }
+
+    public static String utf8ToCp1251(String s) {
+        try {
+            return new String(s.getBytes("UTF8"), "CP1251");
+        } catch (UnsupportedEncodingException e) {
+            return s;
+        }
+    }
+}
