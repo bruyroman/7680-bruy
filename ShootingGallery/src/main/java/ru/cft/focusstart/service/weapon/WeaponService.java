554dddc88fcd84f221d0a28e921574f796e655aa
bruyroman <bruyroman@ya.ru>
2020-01-10 19:46:31 +0700

    реализован интерфейс сервисов

---
+++ ShootingGallery/src/main/java/ru/cft/focusstart/service/weapon/WeaponService.java
@@ -0,0 +1,16 @@
+﻿package ru.cft.focusstart.service.weapon;
+
+import ru.cft.focusstart.api.dto.WeaponDto;
+
+import java.util.List;
+
+public interface WeaponService {
+
+    List<WeaponDto> get(String type, String model, String fullNameInstructor);
+
+    WeaponDto getById(Long id);
+
+    WeaponDto create(WeaponDto weaponDto);
+
+    WeaponDto merge(Long id, WeaponDto instructorDto);
+}
