9fbd6f7454624e0202526a3d53dfd88435f69d75
bruyroman <bruyroman@ya.ru>
2020-01-11 00:47:24 +0700

    реализованы методы конвертирования entity в dto

---
+++ ShootingGallery/src/main/java/ru/cft/focusstart/mapper/InstructorMapper.java
@@ -0,0 +1,26 @@
+﻿package ru.cft.focusstart.mapper;
+
+import ru.cft.focusstart.api.dto.InstructorDto;
+import ru.cft.focusstart.entity.Instructor;
+
+public class InstructorMapper {
+
+    private static final InstructorMapper INSTANCE = new InstructorMapper();
+
+    private InstructorMapper() {}
+
+    public static InstructorMapper getInstance() {
+        return INSTANCE;
+    }
+
+    public InstructorDto toDto(Instructor instructor) {
+        return InstructorDto.builder()
+                .setId(instructor.getId())
+                .setSurname(instructor.getPerson().getSurname())
+                .setName(instructor.getPerson().getName())
+                .setPatronymic(instructor.getPerson().getPatronymic())
+                .setBirthdate(instructor.getPerson().getBirthdate())
+                .setCategory(instructor.getCategory())
+                .build();
+    }
+}
