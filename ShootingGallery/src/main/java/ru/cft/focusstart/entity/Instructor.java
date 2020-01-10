499659bac566865803c2151932fe6a8adf07bc79
bruyroman <bruyroman@ya.ru>
2020-01-11 00:20:48 +0700

    Реализовано entity

---
+++ ShootingGallery/src/main/java/ru/cft/focusstart/entity/Instructor.java
@@ -0,0 +1,77 @@
+﻿package ru.cft.focusstart.entity;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Instructor {
+
+    private Long id;
+    private Person person;
+    private InstructorCategory category;
+    private List<Visit> visits;
+    private List<Weapon> weapons;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public InstructorCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(InstructorCategory category) {
+        this.category = category;
+    }
+
+    public List<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(List<Visit> visits) {
+        this.visits = visits;
+    }
+
+    public List<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(List<Weapon> weapons) {
+        this.weapons = weapons;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Instructor instructor = (Instructor) o;
+        return Objects.equals(id, instructor.id) &&
+                Objects.equals(person, instructor.person) &&
+                Objects.equals(category, instructor.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, person, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", person=" + person +
+                ", category='" + category  + '\'' +
+                '}';
+    }
+}
---
+++ ShootingGallery/src/main/java/ru/cft/focusstart/entity/Visit.java
@@ -0,0 +1,92 @@
+﻿package ru.cft.focusstart.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Visit {
+
+    private Long id;
+    private Person client;
+    private Instructor instructor;
+    private Weapon weapon;
+    private LocalDateTime datetimeStart;
+    private LocalDateTime datetimeEnd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getClient() {
+        return client;
+    }
+
+    public void setClient(Person client) {
+        this.client = client;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public LocalDateTime getDatetimeStart() {
+        return datetimeStart;
+    }
+
+    public void setDatetimeStart(LocalDateTime datetimeStart) {
+        this.datetimeStart = datetimeStart;
+    }
+
+    public LocalDateTime getDatetimeEnd() {
+        return datetimeEnd;
+    }
+
+    public void setDatetimeEnd(LocalDateTime datetimeEnd) {
+        this.datetimeEnd = datetimeEnd;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Visit visit = (Visit) o;
+        return Objects.equals(id, visit.id) &&
+                Objects.equals(client, visit.client) &&
+                Objects.equals(instructor, visit.instructor) &&
+                Objects.equals(weapon, visit.weapon) &&
+                Objects.equals(datetimeStart, visit.datetimeStart) &&
+                Objects.equals(datetimeEnd, visit.datetimeEnd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, client, instructor, weapon, datetimeStart, datetimeEnd);
+    }
+
+    @Override
+    public String toString() {
+        return "Visit{" +
+                "id=" + id +
+                ", client=" + client +
+                ", instructor=" + instructor +
+                ", weapon=" + weapon +
+                ", datetimeStart='" + datetimeStart + '\'' +
+                ", datetimeEnd='" + datetimeEnd + '\'' +
+                '}';
+    }
+}
