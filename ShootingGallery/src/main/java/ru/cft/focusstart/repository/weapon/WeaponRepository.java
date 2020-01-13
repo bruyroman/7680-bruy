a3a011dabcb59a340d2036acd0667448a4d8298e
bruyroman <bruyroman@ya.ru>
2020-01-13 15:59:13 +0700

    созданы интерфейсы для работы с репозиторием

---
+++ ShootingGallery/src/main/java/ru/cft/focusstart/repository/weapon/WeaponRepository.java
@@ -0,0 +1,17 @@
+﻿package ru.cft.focusstart.repository.weapon;
+
+import ru.cft.focusstart.entity.Weapon;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WeaponRepository {
+
+    List<Weapon> get(String type, String model, String fullNameInstructor);
+
+    Optional<Weapon> getById(Long id);
+
+    void add(Weapon weapon);
+
+    void update(Weapon weapon);
+}
