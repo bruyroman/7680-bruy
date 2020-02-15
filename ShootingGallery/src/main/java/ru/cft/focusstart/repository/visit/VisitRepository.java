0cabe936b1f7f52512b847246798d97b53637d1e
bruyroman <bruyroman@ya.ru>
2020-02-16 00:17:33 +0700

    - Уточнение логики по визитам согласно API  - Устранение необаснованного ограничения загрузки инструктора и оружия у визита

--- ShootingGallery/src/main/java/ru/cft/focusstart/repository/visit/VisitRepository.java
+++ ShootingGallery/src/main/java/ru/cft/focusstart/repository/visit/VisitRepository.java
@@ -12,8 +12,8 @@ public interface VisitRepository extends JpaRepository<Visit, Long> {
     String GET_BY_FULLNAME_AND_DATETIME_START_AND_END_QUERY =
             "select v from Visit v join fetch v.client " +
                     "where lower(concat(v.client.surname, ' ', v.client.name, ' ', coalesce(v.client.patronymic, ''))) like lower(concat('%', :fullNameClient, '%')) " +
-                    " and (:dateTimeFrom = v.datetimeStart or cast(:dateTimeFrom as date) is null)" +
-                    " and (:dateTimeTo = v.datetimeEnd or cast(:dateTimeTo as date) is null)";
+                    " and (cast(:dateTimeFrom as date) is null or :dateTimeFrom < v.datetimeStart or :dateTimeFrom < v.datetimeEnd)" +
+                    " and (cast(:dateTimeTo as date) is null or :dateTimeTo > v.datetimeStart or :dateTimeTo > v.datetimeEnd)";
 
     @Query(value = GET_BY_FULLNAME_AND_DATETIME_START_AND_END_QUERY)
     List<Visit> find(LocalDateTime dateTimeFrom, LocalDateTime dateTimeTo, String fullNameClient);
